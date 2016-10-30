7f21b514cdeb793e061d83f229ccf313bdeb34fc
yefeng <yefeng87710@gmail.com>
2016-10-30 10:18:31 +0800

    tag v0.2 -m "templates"

--- app/views.py
+++ app/views.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    user = {'nickname':'Keqing'}
+    posts = [
+        {
+            'author':{'nickname': 'John'},
+            'body': 'Beautiful day in Wuxue!'
+        },
+        {
+            'author':{'nickname': 'Susan'},
+            'body': 'Beautiful day in Daye!'
+        }
+    ]
+    return render_template('index.html',
+            title = '首页',
+            user =user,
+            posts = posts
+    )
